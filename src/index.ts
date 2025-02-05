ccf76b8c71d73710e6051dea3aa9f09696e8fb15
ajeeshRS <ajeeshrs79@gmail.com>
2025-02-05 17:01:12 +0530

    added endpoint

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ app.get("/hai", (req, res) => {
   res.send("hai from port 8080");
 });
 
+app.get("/name", (req, res) => {
+  res.send("hey! its me ajeesh");
+});
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
