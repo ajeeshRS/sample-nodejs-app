63b2ab76cd47ebd0539973da5df73ff244367709
ajeeshRS <ajeeshrs79@gmail.com>
2025-02-05 18:21:10 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ app.get("/name", (req, res) => {
   res.send("hey! its me ajeesh");
 });
 
+app.get("/todo", (req, res) => {
+  res.send("you dont have any todo bruh!");
+});
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
